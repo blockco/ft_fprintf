f3cbcf9ad5af0913cc7076c934327ec5fc79d787
robert passafaro <rpassafa@e1z2r7p8.42.us.org>
2016-11-05 14:56:29 -0700

    pushing format changes

--- ft_printf.h
+++ ft_printf.h
@@ -9,11 +9,11 @@
 extern char *g_format;
 extern void (*g_gl[14])(va_list *ptr);
 
-void pf_putstr(va_list *ptr);
-void pf_putnbr(va_list *ptr);
-void pf_putchar(va_list *args);
-void pf_x_handle(va_list *args);
-void pf_o_handle(va_list *args);
+void 	pf_putstr(va_list *ptr);
+void 	pf_putnbr(va_list *ptr);
+void 	pf_putchar(va_list *args);
+void 	pf_x_handle(va_list *args);
+void 	pf_o_handle(va_list *args);
 char	*ft_itoa_base(int value, int base);
 int		ft_pow(int nb, int pow);
 #endif
